e4752f05735f91e06d4537ad9ea8252c7669bc38
Plasnid <stuart@sackler.ca>
2019-10-17 22:40:30 -0400

    initial file upload

---
+++ script.js
@@ -0,0 +1,71 @@
+/**
+ * Test 1:  Total 50 marks
+ * 
+ *  Question 1. (5 marks)
+ * 
+ *  initialize an npm project for this project
+ * 
+ *  What is the npm command to make package.json?
+ *  write the command in your javascript notes then
+ *  run the command to make package.json.
+ */
+
+/**
+ * Question 2. (2 marks)
+ * 
+ *  add lite-server to your project
+ * 
+ *  What is the npm command to add lite-server
+ *  as a development dependency for your project?
+ *  
+ *  Run the command to add lite-server to your project.
+ */
+
+/**
+ *  Question 3. (10 marks)
+ * 
+ *  There are two scripts/task runners that we have used to 
+ *  run projects.  lite and start.
+ * 
+ *  Add the two scripts(task runners) to your project, 
+ *  and run the npm command to use the "start" task runer
+ */
+
+/**
+ * Question 4. (3 marks)
+ * Link the javascript to the html page
+ */
+
+/**
+ *  Question 5.(20 marks total)
+ *  
+ *  ES5 Classes:  Patient to See you!
+ *  Make an ES5 class called DoctorsOffice
+ * 
+ *  Your class must contain:
+ *      -a value called maxPatients(3 marks)
+ *          -make this a number between 5 and 20
+ *      -an array of 6 objects representing pretend health issues(3 marks)
+ *          -each object must contain
+ *              -a pretend illness(you choose)
+ *              -a value for length of treatment
+ *      -an array of 6 pretend names (3 marks)
+ *      -a function called seePatients(11 marks)
+ *          -your function must
+ *              -contain an interval that will:
+ *                  -select a patient name from the names array
+ *                  -select a patient illness and treatment from the health issues array
+ *                  -console using console.log and template literals make a console log with a
+ *                      -first name
+ *                      -last name
+ *                      -illness
+ *                      -treatment
+ *                  -check if you have seen the max number of patients
+ *                      -if you have, then call a function to end the interval
+ *      -the seePatients, and functions to stop your interval must be in the prototype.
+ */
+
+/**
+ * Question 6:(10 marks)
+ *  Make an ES6 class that has the same functionality as your ES5 class
+ */
